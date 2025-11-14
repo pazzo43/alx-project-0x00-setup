3295a5dbc1c1b308430602dda17bb515216a98b2
Patrick NDICUNGUYE <pazzondicunguye43@gmail.com>
2025-11-14 19:45:23 +0200

    Update index.ts

--- interfaces/index.ts
+++ interfaces/index.ts
@@ -7,3 +7,35 @@ export interface ButtonProps {
   title: string;
   styles: string;
 }
+
+pages/landing.tsx
+pages/about.tsx
+pages/
+ ├── api/
+ ├── _app.tsx
+ ├── _document.tsx
+ ├── index.tsx
+ ├── landing.tsx
+ └── about.tsx
+
+const Landing: React.FC = () => {
+  return (
+    <div>
+      <h1 className="text-xl font-extralight">Landing Page</h1>
+    </div>
+  );
+};
+
+export default Landing;
+const About: React.FC = () => {
+  return (
+    <div>
+      <h1 className="text-xl font-extralight">About Page</h1>
+    </div>
+  );
+};
+
+export default About;
+npm run dev -- -p 3000
+http://localhost:3000
+
