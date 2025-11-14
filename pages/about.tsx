50c374711705817cba8188919af48911daccb49e
Patrick NDICUNGUYE <pazzondicunguye43@gmail.com>
2025-11-14 19:39:21 +0200

    Update about.tsx

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,1 +1,19 @@
+pages/
+ ├── api/
+ ├── _app.tsx
+ ├── _document.tsx
+ └── index.tsx
+pages/about.tsx
+// pages/about.tsx
+
+const About = () => {
+  return (
+    <div>
+      <h1>About Page</h1>
+      <p>This is the about page.</p>
+    </div>
+  );
+};
+
+export default About;
 
