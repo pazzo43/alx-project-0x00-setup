8b1fd3e20b98e760fd32ff2c28853b74c5b28672
Patrick NDICUNGUYE <pazzondicunguye43@gmail.com>
2025-11-14 20:48:35 +0200

    Update Pill.tsx

--- components/Pill.tsx
+++ components/Pill.tsx
@@ -1,3 +1,6 @@
+import { PillProps } from "@/interfaces";
+
+
 const Pill: React.FC = () => {
   return (
     <div className="flex justify-center items-center bg-[#F9F9F9] px-3 w-auto h-[27px] rounded-full">
@@ -10,4 +13,16 @@ export default Pill;
 npm run dev -- -p 3000
 http://localhost:3000
 http://localhost:3000/landing
+const Pill: React.FC<PillProps> = ({ title }) => {
+  return (
+    <div className="flex justify-center items-center bg-[#F9F9F9] px-3 w-auto h-[27px] rounded-full">
+      <p className="text-sm">{title}</p>
+    </div>
+  );
+};
+
+export default Pill;
+npm run dev -- -p 3000
+http://localhost:3000/landing
+
 
